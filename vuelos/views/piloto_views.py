f3420fd8fe5653a418a50e321106549233ffabad
dylan18x <gadieldylan02@gmail.com>
2026-07-13 09:43:28 -0500

    Correxcion de Errores

--- vuelos/views/piloto_views.py
+++ vuelos/views/piloto_views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from vuelos.models.piloto import Piloto
 from vuelos.serializers.piloto_serializer import PilotoSerializer
+from vuelos.permissions import IsStaffOrReadOnly
 
 class PilotoViewSet(viewsets.ModelViewSet):
     queryset = Piloto.objects.all()
