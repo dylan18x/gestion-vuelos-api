68cebcf473f3a0f4db459a324b39cd3e8c443fe9
dylan18x <gadieldylan02@gmail.com>
2026-07-13 23:46:51 -0500

    Auth cambio

--- vuelos/serializers/auth.py
+++ vuelos/serializers/auth.py
@@ -10,14 +10,22 @@ class CustomTokenSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         token['email']    = user.email
         token['is_staff'] = user.is_staff
+        group = user.groups.first()
+        token['role'] = group.name if group else None
         return token
 
     def validate(self, attrs):
         data = super().validate(attrs)
-        data['user_id']  = self.user.id
-        data['username'] = self.user.username
-        data['email']    = self.user.email
-        data['is_staff'] = self.user.is_staff
+        group = self.user.groups.first()
+
+        data['user_id']   = self.user.id
+        data['id']        = self.user.id          # 👈 para que Flutter lo reconozca
+        data['username']  = self.user.username
+        data['email']     = self.user.email
+        data['first_name']= self.user.first_name   # 👈 opcional, para el saludo
+        data['last_name'] = self.user.last_name    # 👈 opcional
+        data['is_staff']  = self.user.is_staff
+        data['role']      = group.name if group else None  # 👈 el que faltaba
         return data
 
 
